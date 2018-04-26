0bb99a27a19c361763ae462f74b1f9b4eca2f457
Luis Alfredo León Villapún <leonvillapun@gmail.com>
2018-04-26 17:14:51 -0500

    Hola

---
+++ LaboratorioUrbanoSensorialTests/LaboratorioUrbanoSensorialTests.swift
@@ -0,0 +1,36 @@
+//
+//  LaboratorioUrbanoSensorialTests.swift
+//  LaboratorioUrbanoSensorialTests
+//
+//  Created by Luis Alfredo León Villapún on 20/04/18.
+//  Copyright © 2018 Luis Alfredo León Villapún. All rights reserved.
+//
+
+import XCTest
+@testable import LaboratorioUrbanoSensorial
+
+class LaboratorioUrbanoSensorialTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
